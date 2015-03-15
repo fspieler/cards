850f070b48b732654ab3e0efb960c6d57d4170a8
al <almno10@gmail.com>
2015-03-15 00:58:38 -0400

    added User.hole_cards()

--- lib/user.py
+++ lib/user.py
@@ -1,9 +1,10 @@
-#/usr/bin/env python
+#/usr/bin/env python3
 
 class User(object):
     def __init__(self,user_id=None):
         if user_id == None:
             self.new_user = True
+            user_id = time.time()
         else:
             self.new_user = False
             #fetch all records from db about user_id
@@ -13,12 +14,14 @@ class User(object):
         if self.new_user:
             pass
         else:
-        
             #Do UPDATES 
             pass
+    
+    def hole_cards():
+        # fetch cards for user_id
+        pass
         
     def delete(self):
         if self.new_user == False:
             return False
-        
             # Delete user code here
