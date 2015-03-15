66146f82700b80c36dd68ccface0399ae2c9cb21
al <almno10@gmail.com>
2015-03-15 00:14:55 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import itertools, random 
+
+# create deck
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club'])) 
+# shuffle deck
+random.shuffle(deck) 
+# draw five cards 
+print("You got:") 
+for i in range(5): 
+    print(deck[i][0], "of", deck[i][1])
+
+
+
+
+
