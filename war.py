0fba84fea43f6376fed8cb3559d1cbd473050948
Fred Spieler <fred@fredspieler.com>
2015-03-18 01:46:11 -0400

    Crappy war impl

--- war.py
+++ war.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
 
-from lib.card import *
+from pokerHands import *
 
-you = []
-them = []
+def war(you, them, cards=None):
+    if(cards is None):
+        cards=OrderedCards()
+    yourCard = you.list()[0]
+    you.deal(1,cards)
+    theirCard = them.list()[0]
+    them.deal(1,cards)
+    print("Your card: " + str(yourCard))
+    print("Their card: " + str(theirCard))
+    if(yourCard > theirCard):
+        print("You win this round and these cards: " + str(cards))
+        cards.deal(len(cards),you)
+        print("Card count: you -> " + str(len(you)) + ", them -> " + str(len(them)))
+    elif(yourCard < theirCard):
+        print("They win this round and these cards: " + str(cards))
+        cards.deal(len(cards),them)
+        print("Card count: you -> " + str(len(you)) + ", them -> " + str(len(them)))
+    else:
+        print("WAR!")
+        you.deal(3, cards)
+        them.deal(3, cards)
+        war(you, them, cards)
+
+you = OrderedCards()
+them = OrderedCards()
 players = [you, them]
 deck= getDeck()
 
-deal(deck, players, int(52/len(players)))
+deck.deal(26, you, them)
 
 print("you:")
-printCards(you)
+print(you)
 
 print("them:")
-printCards(them)
+print(them)
 
+while len(you) > 0 and len(them) > 0:
+    war(you, them)
